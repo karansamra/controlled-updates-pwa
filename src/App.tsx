d444e89f685acf16c70a46b13661863022d79e51
Jaskaran Singh <karansamra.it@gmail.com>
2023-11-22 21:08:48 +0530

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import logo from './gear.png';
+import './App.css';
+
+interface AppProps {
+  appUpdatePending: boolean;
+  updateAction: () => void;
+}
+
+export default function App(props: AppProps) {
+  const { appUpdatePending, updateAction } = props;
+  const [showUpdateBtn, setShowUpdateBtn] = useState(appUpdatePending);
+
+  useEffect(() => {
+    setShowUpdateBtn(appUpdatePending);
+  }, [appUpdatePending]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {showUpdateBtn ?
+          <Button label="Click to Update" action={() => updateAction()} /> : null
+        }
+        <p>
+          This is a PWA!
+        </p>
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="*" element={<Home />} />
+          </Route>
+        </Routes>
+      </div>
+      <footer>
+        <p>
+          Version: {process.env.REACT_APP_VERSION_ID || 'development'}
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+function Button({ label = '', action = () => { } }) {
+  const onClick = () => action();
+  return (
+    <button className="Update-btn" onClick={onClick}>{label}</button>
+  );
+}
+
+function Layout() {
+  return (
+    <div>
+      <nav>
+        <ul className='App-nav'>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
---
+++ src/index.tsx
@@ -0,0 +1,73 @@
+import React, { FC, useEffect, useState } from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+import { BrowserRouter, useLocation } from "react-router-dom";
+
+// wrapper component to add in service worker details.
+interface WrappedCompProps {
+  appUpdatePending: boolean;
+  updateAction: () => void;
+};
+const withSwRegistration = (WrappedComp: FC<WrappedCompProps>,) => {
+  return () => {
+    // holds all the SW registration setup
+    const [appUpdatePending, setAppUpdatePending] = useState(false);
+    const location = useLocation();
+    // updates the state when a new update is pending.
+    const onSWUpdate = () => {
+      setAppUpdatePending(!appUpdatePending);
+    };
+    // action for updating the service worker.
+    const updateAction = () => {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then((registration) => {
+          if (registration.waiting) {
+            // send the skip message to kick off the service worker install.
+            registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+            // add an listener to reload page when the new service worker is ready.
+            registration.waiting.addEventListener('statechange', (event: Event) => {
+              const { state = '' } = event.target as unknown as { state: string; } || {};
+              if (state === 'activated') {
+                window.location.reload();
+              }
+            });
+          }
+        });
+      }
+    };
+    // effect added from router location to check for a new service worker on every
+    // page transition (change of route). 
+    useEffect(() => {
+      if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.ready.then((registration) => {
+          registration.update();
+        });
+      }
+    }, [location]);
+
+    // registers the service worker based on config setting.
+    if (process.env.REACT_APP_REGISTER_SW === "true") {
+      serviceWorkerRegistration.register({ onUpdate: onSWUpdate });
+    } else {
+      serviceWorkerRegistration.unregister();
+    }
+    return (
+      <WrappedComp updateAction={updateAction} appUpdatePending={appUpdatePending} />
+    );
+  };
+};
+
+const AppWithSwRegistration = withSwRegistration(App);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <AppWithSwRegistration />
+    </BrowserRouter>
+  </React.StrictMode>
+);
